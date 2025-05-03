f909f048ffe656291fbbae20ec0148a8e07f97b0
natsuki <natsuki.ss@icloud.com>
2025-05-03 13:38:00 +0900

    APIプロジェクトの初期設定を追加。必要なファイル（.gitignore、package.json、README.md、tsconfig.json、Prismaスキーマなど）を作成し、依存関係を設定。Prismaクライアントを使用してTodoモデルを定義し、基本的なAPIエンドポイントを実装。

---
+++ api/src/index.ts
@@ -0,0 +1,27 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+import { PrismaClient } from '../generated/prisma/index.js'
+
+const app = new Hono()
+const prisma =new PrismaClient()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/system/ping', (c) => {
+  return c.json({ message: 'pong' })
+})
+
+// 一覧取得
+app.get('/todos', async (c) => {
+  const todos = await prisma.todo.findMany()
+  return c.json(todos)
+})
+
+serve({
+  fetch: app.fetch,
+  port: 3000
+}, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`)
+})
